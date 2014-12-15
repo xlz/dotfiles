440fe5d1a1cebe57fee96ca684f5749d21321e64
Lingzhu Xiang <lingzhu@lingzhuxiang.com>
2014-12-15 16:26:46 -0500

    Update drm.sh

--- home/bin/drm.sh
+++ home/bin/drm.sh
@@ -4,7 +4,7 @@ export XAUTHORITY=/home/xlz/.Xauthority
 
 read status </sys/class/drm/card0-DP-2/status
 if [ "$status" = connected ]; then
-	xrandr --output DP2 --auto --same-as eDP1
+	xrandr --output eDP1 --auto --primary --output DP2 --auto --same-as eDP1
 else
 	xrandr --output DP2 --off
 fi
